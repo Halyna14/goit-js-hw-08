e67cda77ea119954e6474775b9d723ee73a5c0cd
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-11-30 22:43:59 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,20 @@
 import Player from '@vimeo/player';
 import Throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = 'videoplayer-current-time';
+
+player.on('timeupdate', Throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(currentTime, seconds);
+}
+setCurrentTime();
+function setCurrentTime() {
+  if (!localStorage.getItem(currentTime)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(currentTime));
+}
