d61e073cb747e01fc9f81cb55063f13c1659551c
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-12-01 23:25:39 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,44 @@
 import Throttle from 'lodash.throttle';
 
-const feedbackForm ='feedback-form-state';
 const form = document.querySelector('.feedback-form');
+const email = form.querySelector("[name='email']");
+const message = form.querySelector("[name='message']");
+
+const LOCAL_KEY = 'feedback-form-state';
+
+form.addEventListener('input', Throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+let saveText = JSON.parse(localStorage.getItem('feedback-form-state'));
+populateOnForm();
+function onInput(event) {
+  if (!saveText) {
+    const onForm = {};
+    onForm[event.target.name] = event.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(onForm));
+  } else {
+    const onForm = { ...saveText };
+    onForm[event.target.name] = event.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(onForm));
+  }
+}
+function onSubmit(event) {
+  event.preventDefault();
+
+  try {
+    console.log(saveText);
+  } catch (error) {
+    console.log(error.email);
+    console.log(error.text);
+  }
+  localStorage.removeItem(LOCAL_KEY);
+  saveText = '';
+
+  event.target.reset();
+}
+function populateOnForm() {
+  if (saveText) {
+    onInput.value = saveText.email || '';
+    onSubmit.value = saveText.message || '';
+  }
+}
